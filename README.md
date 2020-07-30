53d534f72d061cb0f1dd07d1a08de11eb315435d
fanhuangrili <445695121@qq.com>
2020-07-30 14:39:23 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # SpecialAdditionalDeduction
 专项附加扣除数据提取
+
+Bug待修复:
+错误身份证号码读写错误;
+实际环境中不存在这种情况
+
+先安装环境:
+
+```
+pip install -r requirements.txt
+```
+
+运行
+
+```
+python 附加专项扣除数_20200424_1013.py
+```
+
+数据提取到**text.xlsx**中，然后把数据放到模板中，模板公式按实际情况更改。
+
+详情见[薪酬核算之个人所得税 - 泛黄日历的文章 - 知乎](https://zhuanlan.zhihu.com/p/142042877) 
---
+++ 附加专项扣除数_20200424_1013.py
@@ -0,0 +1,103 @@
+import glob
+import json
+import os
+import re
+import sys
+
+import pandas as pd
+
+import xlrd
+
+
+def get_row_col(cell_str, col_offset=0):
+    col_r = r"[0-9]{1,}"
+    row_r = r"[A-Z]{1,}"
+    return (
+        int(re.findall(col_r, cell_str)[0]) - 1 + col_offset,
+        ord(re.findall(row_r, cell_str)[0]) - ord("A"),
+    )
+
+
+if __name__ == "__main__":
+    # path = sys.path[-1]
+    # 获取所有的文件
+    path = sys.argv[0]
+    print(path)
+    if os.path.isfile(path):
+        path = os.path.dirname(path)
+    files = glob.glob(path + os.sep + "source" + os.sep + "*.xls")
+    # 按修改时间排序
+    files = sorted(files, key=lambda x: os.stat(x).st_mtime)
+    print(files)
+    # 存储数据
+    data_dict = {}
+    for index, file in enumerate(files):
+        # 读取excel数据
+        print(file)
+        data = xlrd.open_workbook(file)
+        table = data.sheets()[0]
+        # # 身份证号码
+        # print(table.cell_value(get_row_col("C6")[0], get_row_col("C6")[1]))
+        # # 本月子女支出(1000,看百分比)
+        # print(table.cell_value(get_row_col("G11")[0], get_row_col("G11")[1]))
+        # print(table.cell_value(get_row_col("G14")[0], get_row_col("G14")[1]))
+        # # 继续教育支出(400每条)
+        # print(table.cell_value(get_row_col("G17")[0], get_row_col("G17")[1]))
+        # # 住房贷款(1000)
+        # print(table.cell_value(get_row_col("G23")[0], get_row_col("G23")[1]))
+        # # 住房租金支出(1500)
+        # print(table.cell_value(get_row_col("G32")[0], get_row_col("G32")[1]))
+        # # 赡养老人支出(2000封顶,按百分比)
+        # print(table.cell_value(get_row_col("G38")[0], get_row_col("G38")[1]))
+        col_offset = 0
+        if "子女" in table.cell_value(get_row_col("A11")[0], get_row_col("A11")[1]):
+            col_offset = 0
+        if "子女" in table.cell_value(get_row_col("A15")[0], get_row_col("A15")[1]):
+            col_offset = 4
+        if "子女" in table.cell_value(get_row_col("A19")[0], get_row_col("A19")[1]):
+            col_offset = 8
+        data_dict[index] = {
+            "身份证号码": table.cell_value(get_row_col("C6")[0], get_row_col("C6")[1]),
+            "本月子女支出1": table.cell_value(
+                get_row_col("G14", 0)[0], get_row_col("G14", 0)[1]
+            ),
+            "本月子女支出2": table.cell_value(
+                get_row_col("G14", 4)[0], get_row_col("G14", 4)[1]
+            ),
+            "本月子女支出3": table.cell_value(
+                get_row_col("G14", 8)[0], get_row_col("G14", 8)[1]
+            ),
+            # 根据子女支出的条数, 确定偏移量
+            "继续教育支出1": table.cell_value(
+                get_row_col("G17", col_offset)[0], get_row_col("G17", col_offset)[1]
+            ),
+            "继续教育支出2": table.cell_value(
+                get_row_col("G18", col_offset)[0], get_row_col("G18", col_offset)[1]
+            ),
+            "住房贷款": table.cell_value(
+                get_row_col("G24", col_offset)[0], get_row_col("G24", col_offset)[1]
+            ),
+            "住房租金支出": table.cell_value(
+                get_row_col("C34", col_offset)[0], get_row_col("C34", col_offset)[1]
+            ),
+            "赡养老人支出": table.cell_value(
+                get_row_col("G38", col_offset)[0], get_row_col("G38", col_offset)[1]
+            ),
+        }
+    data_pd = pd.read_json(json.dumps(data_dict), orient="index")
+    # 排序
+    data_pd = data_pd[
+        [
+            "身份证号码",
+            "本月子女支出1",
+            "本月子女支出2",
+            "本月子女支出3",
+            "继续教育支出1",
+            "继续教育支出2",
+            "住房贷款",
+            "住房租金支出",
+            "赡养老人支出",
+        ]
+    ]
+    data_pd["身份证号码"] = data_pd["身份证号码"].astype("str")
+    data_pd.to_excel("{filename}.xlsx".format(filename="text"), index=False)
